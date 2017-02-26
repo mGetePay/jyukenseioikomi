f2dd706b7a32b76a166d06534266c160234bdfa3
ドコモ <docomo@dokomo-no-macbook-air.local>
2017-02-26 22:16:17 +0900

    Initial Commit

---
+++ 受験生追込みアプリ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  受験生追込みアプリ
+//
+//  Created by ドコモ on 2017/02/26.
+//  Copyright © 2017年 Keita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
