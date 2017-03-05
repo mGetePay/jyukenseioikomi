94f66bd41d1e3aff544ba00e07c79fd26321c9b4
ドコモ <docomo@dokomo-no-macbook-air.local>
2017-03-05 14:29:48 +0900

    受験生追込みアプリ作成
    初回アップロード
    ・UI
    ・今日勉強した時間の合計値を取得できる機能

--- 受験生追込みアプリ/ViewController.swift
+++ 受験生追込みアプリ/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var num:Double = 0
+    
+    @IBOutlet weak var todayStudiedTime_label: UILabel!
+    @IBOutlet weak var studiedTime_label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       //一旦各labelに空文字を入力
+    todayStudiedTime_label.text = ""
+    studiedTime_label.text = ""
+        
     }
 
+    @IBAction func studyTimeButton_pushed(_ sender: UIButton) {
+        todayStudiedTime_label.text! = sender.titleLabel!.text!
+        if Double(todayStudiedTime_label.text!) != nil {
+        num += Double(Double(todayStudiedTime_label.text!)!)
+        
+        todayStudiedTime_label.text = String( num)
+        studiedTime_label.text = String(num)
+ print(num)
+        }
+    }
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
