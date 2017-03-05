c5fbaa430efb4a973a3dd5bca75e7ccc55fc4063
ドコモ <docomo@dokomo-no-macbook-air.local>
2017-03-05 22:05:28 +0900

    受験生追込みアプリr１
    勉強時間の合計を保存する機能を模索中

--- 受験生追込みアプリ/ViewController.swift
+++ 受験生追込みアプリ/ViewController.swift
@@ -11,18 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     var num:Double = 0
-    
+    var testNum:Double = 0
+    var testNum2:Double = 0
+    let defaults = UserDefaults.standard
+
+    //labelを準備
     @IBOutlet weak var todayStudiedTime_label: UILabel!
     @IBOutlet weak var studiedTime_label: UILabel!
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-       //一旦各labelに空文字を入力
+    super.viewDidLoad()
+    
+    //一旦各labelに空文字を入力
     todayStudiedTime_label.text = ""
     studiedTime_label.text = ""
-        
     }
 
+    //勉強した時間を入力し、その合計時間を出力する
     @IBAction func studyTimeButton_pushed(_ sender: UIButton) {
         todayStudiedTime_label.text! = sender.titleLabel!.text!
         if Double(todayStudiedTime_label.text!) != nil {
@@ -30,10 +35,17 @@ class ViewController: UIViewController {
         
         todayStudiedTime_label.text = String( num)
         studiedTime_label.text = String(num)
- print(num)
-        }
+        testNum = Double(Double(studiedTime_label.text!)!)
+
+ print(testNum)
+     defaults.set(testNum, forKey:"Double")
+     defaults.synchronize()
+     testNum2 = defaults.double(forKey: "Double")
+print(testNum2)
+         }
     }
 
+ 
     
 
     
