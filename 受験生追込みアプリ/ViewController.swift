2f9008af38cbf8320d66b3d6ce68875063f96eff
ドコモ <docomo@dokomo-no-macbook-air.local>
2017-03-08 00:14:57 +0900

    受験生追い込みアプリr４
    ひとまず値の保存ができた。
    しかし再ビルドすると値が上書き（リセット）されるため要修正。

--- 受験生追込みアプリ/ViewController.swift
+++ 受験生追込みアプリ/ViewController.swift
@@ -9,74 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
-
-
     
+    //変数を準備し初期値を入力する
     var num:Double = 0
     var testNum:Double = 0
-//    var testNum2:Double = 0
-//    let defaults = UserDefaults.standard
     var testNum2 = UserDefaults.standard
-
     
-    //labelを準備
+    //labelを準備する
     @IBOutlet weak var todayStudiedTime_label: UILabel!
     @IBOutlet weak var studiedTime_label: UILabel!
-    
-    
+
     override func viewDidLoad() {
     super.viewDidLoad()
     
-    //一旦各labelに空文字を入力
-    todayStudiedTime_label.text = ""
-        
-//    studiedTime_label.text = ""
-        
-    //文字列が保存されている場合はラベルに文字列を設定する。
-//    if testNum2.string(forKey:"testNum2") != nil {
-//    studiedTime_label.text = String(describing: self.testNum2)
-    
-//    if let testNum2 = testNum2.string(forKey: "testNum2") {
-//    studiedTime_label.text = String(testNum2)
-//        }
-
-    studiedTime_label.text = testNum2.string(forKey: "testNum2")
-       
-        
+    //キー"testNum"にtestNum2の値(Double)を文字列（String）に変換して格納
+    studiedTime_label.text = String(testNum2.double(forKey: "testNum"))
     }
 
     //勉強した時間を入力し、その合計時間を出力する
     @IBAction func studyTimeButton_pushed(_ sender: UIButton) {
+        
+        //todayStudiedTime_labelに数値ボタンのタイトル（＝数値）を表示
         todayStudiedTime_label.text! = sender.titleLabel!.text!
+        
+        //num（:Double）に数値ボタンの数値を加算
         if Double(todayStudiedTime_label.text!) != nil {
         num += Double(Double(todayStudiedTime_label.text!)!)
         
+        //todayStudiedTime_labelにnum(:String)を表示
         todayStudiedTime_label.text = String( num)
-        studiedTime_label.text = String(num)
-        testNum = Double(Double(studiedTime_label.text!)!)
 
+        //StudiedTime_labelにnum(:String)を表示
+        studiedTime_label.text = String(num)
+        
+        testNum = Double(Double(studiedTime_label.text!)!)
  print(testNum)
      testNum2.set(testNum, forKey:"testNum")
      testNum2.synchronize()
  print(testNum2)
             
-//            if (testNum2.object(forKey: "Key") != nil) {
-//            print("データ有り") }
-print(UserDefaults.standard.dictionaryRepresentation())
-                
-            
-            
-    
+  
          }
     }
-
-    
-
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+
     }
 
 
